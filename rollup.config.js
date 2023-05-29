7a1a468df7326161d3ad5808d59da7d7056cb373
Anurag Vohra <53807480+anuragvohraec@users.noreply.github.com>
2023-05-29 09:44:48 +0530

    WIP

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+// rollup.config.js
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: './state-them.js',
+  output: {
+    dir: 'dist',
+    paths:{
+      
+    },
+  },
+  plugins: [terser()],
+  external:[],
+    
+};

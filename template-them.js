22daee8e132c3e53cb8135e97e806b1644ae764f
Anurag Vohra <53807480+anuragvohraec@users.noreply.github.com>
2023-06-25 07:15:54 +0530

    WIP

--- template-them.js
+++ template-them.js
@@ -71,6 +71,14 @@ function createStaticIterationList(targetNode){
                     } 
                 }
             }
+            //if its custom element simply skip all its child
+            // if(customElements.get(currentNode.tagName.toLowerCase())){
+            //     let cn = itr.nextNode();
+            //     while(currentNode.contains(cn)){
+            //         cn=itr.nextNode();
+            //     }
+            //     currentNode=cn;
+            // }
         }
         currentNode=itr.nextNode();
     }
@@ -112,6 +120,15 @@ function createNodeListBetween(startCommentNode){
                     } 
                 }
             }
+
+            //if its custom element simply skip all its child
+            // if(customElements.get(currentNode.tagName.toLowerCase())){
+            //     let cn = itr.nextNode();
+            //     while(currentNode.contains(cn)){
+            //         cn=itr.nextNode();
+            //     }
+            //     currentNode=cn;
+            // }
         }
         currentNode = itr.nextNode();
     }
